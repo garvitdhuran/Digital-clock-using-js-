c8cb628bb3456d94d38172303fa343c02f68b487
garvitdhuran <garvitdhuran@gmail.com>
2026-06-13 16:08:24 +0530

    made the time working and added css

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Digital-clock-using-js-
+
+
+Okay so im making a digital clock using js, also ill try to add some features like maybe a timer or something if im able to.
---
+++ script.js
@@ -0,0 +1,23 @@
+const time =document.getElementById("time")
+const timeformat  =  document.getElementById("timeformat")
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    showTime();
+
+    setInterval(showTime, 1000);
+        
+
+});
+
+const showTime = () => {
+    let date = new Date();
+
+    let hr = date.getHours();
+    let sec = date.getSeconds();
+    let min = date.getMinutes();
+
+
+    time.innerHTML = `${hr} : ${min} : ${sec}`;
+    console.log("hours" + hr + "mins" + min + "sec" + sec );
+}
+
